5501c7fd72a1363f921d54c7b471eff21c85aae4
Shafin Ahmed <kazishafin@gmail.com>
2023-10-03 23:55:23 -0400

    Display classes and requirements

--- src/App.js
+++ src/App.js
@@ -20,6 +20,20 @@ function App() {
     setAttributeStats(updatedAttributeStats);
   };
 
+  const [selectedClass, setSelectedClass] = useState("");
+
+  const toggleClassRequirements = (className) => {
+    selectedClass !== className
+      ? setSelectedClass(className)
+      : setSelectedClass("");
+  };
+
+  const areClassRequirementsMet = (className) =>
+    Object.entries(CLASS_LIST[className]).every(
+      ([attribute, minRequirement]) =>
+        attributeStats[attribute] >= minRequirement
+    );
+
   return (
     <div className="App">
       <header className="App-header">
@@ -41,6 +55,38 @@ function App() {
               </div>
             );
           })}
+        </div>
+
+        <div>
+          <h3>Classes</h3>
+          {Object.keys(CLASS_LIST).map((className) => (
+            <div key={className}>
+              <button
+                onClick={() => toggleClassRequirements(className)}
+                style={{
+                  all: "unset", // remove button stylings
+                  cursor: "pointer",
+                  color: areClassRequirementsMet(className) && "green",
+                }}
+              >
+                {className}
+              </button>
+              {selectedClass === className && (
+                <div>
+                  <h3>Minimum Requirements:</h3>
+                  <div style={{ marginBottom: "12px" }}>
+                    {Object.entries(CLASS_LIST[className]).map(
+                      ([attribute, minRequirement]) => (
+                        <div key={attribute}>
+                          {attribute}: {minRequirement}
+                        </div>
+                      )
+                    )}
+                  </div>
+                </div>
+              )}
+            </div>
+          ))}
         </div>
       </section>
     </div>
