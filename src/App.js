83b7f6706c45400aa27c24056b3bba3277721e22
Shafin Ahmed <kazishafin@gmail.com>
2023-10-03 23:47:13 -0400

    Added state and controls for attributes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import { useState } from 'react';
-import './App.css';
-import { ATTRIBUTE_LIST, CLASS_LIST, SKILL_LIST } from './consts.js';
-
+import { useState } from "react";
+import "./App.css";
+import { ATTRIBUTE_LIST, CLASS_LIST, SKILL_LIST } from "./consts.js";
 
 function App() {
-  const [num, setNum] = useState(0);
+  // make an object out of ATTRIBUTE_LIST to store stat for each attribute
+  const initialAttributeStats = ATTRIBUTE_LIST.reduce((acc, attribute) => {
+    acc[attribute] = 10;
+    return acc;
+  }, {});
+
+  const [attributeStats, setAttributeStats] = useState(initialAttributeStats);
+
+  const handleChangeAttribute = (attribute, amount) => {
+    const updatedAttributeStats = {
+      ...attributeStats,
+      [attribute]: attributeStats[attribute] + amount,
+    };
+
+    setAttributeStats(updatedAttributeStats);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -12,10 +27,20 @@ function App() {
       </header>
       <section className="App-section">
         <div>
-          Value:
-          {num}
-          <button>+</button>
-          <button>-</button>
+          <h3>Attributes</h3>
+          {Object.entries(attributeStats).map(([attribute, stat]) => {
+            return (
+              <div key={attribute}>
+                {attribute}: {stat}
+                <button onClick={() => handleChangeAttribute(attribute, -1)}>
+                  -
+                </button>
+                <button onClick={() => handleChangeAttribute(attribute, 1)}>
+                  +
+                </button>
+              </div>
+            );
+          })}
         </div>
       </section>
     </div>
